9342f0f9f15e734a7b7f850410f381eca3a93321
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-11-14 14:58:16 +0100

    Implemented map and list classes

---
+++ ki-aim-frontend/src/app/core/utils/list.ts
@@ -0,0 +1,39 @@
+export class List<T> {
+    private items: Array<T>;
+
+    constructor(items?: Array<T>) {
+        if (items !== null && items !== undefined) {
+            this.items = items; 
+        } else {
+            this.items = [];
+        }
+    }
+
+
+    size(): number {
+        return this.items.length;
+    }
+
+    add(value: T): void {
+        this.items.push(value);
+    }
+
+    get(index: number): T {
+        return this.items[index];
+    }
+
+    getAll(): Array<T> {
+        return this.items; 
+    }
+
+    contains(value: T): boolean {
+        return this.items.includes(value); 
+    }
+
+    remove(value: T): void {
+        var index = this.items.indexOf(value, 0);
+        if (index > -1) {
+            this.items.splice(index, 1);
+        }
+    }
+}

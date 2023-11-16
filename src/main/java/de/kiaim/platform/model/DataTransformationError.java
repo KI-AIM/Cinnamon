dd54aa5a4807db1a588ac87564f4f0b5feb4a386
Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2023-11-16 15:55:43 +0100

    Added JavaDoc

--- src/main/java/de/kiaim/platform/model/DataTransformationError.java
+++ src/main/java/de/kiaim/platform/model/DataTransformationError.java
@@ -3,11 +3,20 @@ package de.kiaim.platform.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Represents an error during the transformation.
+ */
 @Getter
 @AllArgsConstructor
 public class DataTransformationError {
 
+	/**
+	 * Index of the corresponding value in the list of raw values.
+	 */
 	private final int index;
 
+	/**
+	 * Message describing the error.
+	 */
 	private final String errorMessage;
 }
--- src/main/java/de/kiaim/platform/model/TransformationResult.java
+++ src/main/java/de/kiaim/platform/model/TransformationResult.java
@@ -5,14 +5,27 @@ import lombok.Getter;
 
 import java.util.List;
 
+/**
+ * Represents the result of the transformation from the raw input data into DataSet.
+ */
 @Getter
 @AllArgsConstructor
 public class TransformationResult {
 
+	/**
+	 * The transformed DataSet containing all successful transformed entries.
+	 */
 	private final DataSet dataSet;
 
+	/**
+	 * List of errors containing all rows that could not be transformed.
+	 */
 	private final List<DataRowTransformationError> transformationErrors;
 
+	/**
+	 * Adds a new row the list of rows that could not be transformed.
+	 * @param dataRowTransformationError Error containing the row.
+	 */
 	public void addError(final DataRowTransformationError dataRowTransformationError) {
 		transformationErrors.add(dataRowTransformationError);
 	}

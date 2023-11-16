8791d04df60543901ac004bb577eeefd831f561b
Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2023-11-16 11:22:28 +0100

    Added list of datatypes to the DataConfiguration

--- src/main/java/de/kiaim/platform/model/DataConfiguration.java
+++ src/main/java/de/kiaim/platform/model/DataConfiguration.java
@@ -1,4 +1,17 @@
 package de.kiaim.platform.model;
 
+import de.kiaim.platform.model.data.DataType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
 public class DataConfiguration {
+
+	/**
+	 * Datatype and their order of all objects inside a DataRow.
+	 */
+	final List<DataType> dataTypes;
 }

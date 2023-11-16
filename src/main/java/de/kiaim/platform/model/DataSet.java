a26157185058cbebeed12498529e9254fdc07799
Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2023-11-16 11:23:16 +0100

    Added members to DataSet

--- src/main/java/de/kiaim/platform/model/DataSet.java
+++ src/main/java/de/kiaim/platform/model/DataSet.java
@@ -1,4 +1,16 @@
 package de.kiaim.platform.model;
 
+import de.kiaim.platform.model.data.DataRow;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
 public class DataSet {
+
+	final List<DataRow> dataRows;
+
+	final DataConfiguration dataConfiguration;
 }

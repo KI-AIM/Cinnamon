bf86ef897f310324552ef1701a980413cf6769c0
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-11-17 16:12:37 +0100

    Added empty methods to classes that implement DataProcessor

--- src/main/java/de/kiaim/platform/processor/CsvProcessor.java
+++ src/main/java/de/kiaim/platform/processor/CsvProcessor.java
@@ -1,4 +1,19 @@
 package de.kiaim.platform.processor;
 
-public class CsvProcessor implements DataProcessor{
+import de.kiaim.platform.model.DataConfiguration;
+import de.kiaim.platform.model.TransformationResult;
+
+import java.util.Base64;
+
+public class CsvProcessor extends CommonDataProcessor{
+    @Override
+    public TransformationResult read(Base64 data) {
+        return null;
+    }
+
+    @Override
+    public DataConfiguration estimateDatatypes(Base64 data) {
+        return null;
+    }
+
 }
--- src/main/java/de/kiaim/platform/processor/FhirProcessor.java
+++ src/main/java/de/kiaim/platform/processor/FhirProcessor.java
@@ -1,4 +1,18 @@
 package de.kiaim.platform.processor;
 
+import de.kiaim.platform.model.DataConfiguration;
+import de.kiaim.platform.model.TransformationResult;
+
+import java.util.Base64;
+
 public class FhirProcessor implements DataProcessor{
+    @Override
+    public TransformationResult read(Base64 data) {
+        return null;
+    }
+
+    @Override
+    public DataConfiguration estimateDatatypes(Base64 data) {
+        return null;
+    }
 }

3274ecb85a07eebd61f22c160dcfb0248c4ef9ab
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-11-17 16:12:10 +0100

    Implemented DataProcessor interface

--- src/main/java/de/kiaim/platform/processor/DataProcessor.java
+++ src/main/java/de/kiaim/platform/processor/DataProcessor.java
@@ -1,4 +1,31 @@
 package de.kiaim.platform.processor;
 
+import de.kiaim.platform.model.DataConfiguration;
+import de.kiaim.platform.model.TransformationResult;
+
+import java.util.Base64;
+
 public interface DataProcessor {
+
+    /**
+     * Receives byte data from frontend, converts it to
+     * the corresponding filetype and performs the
+     * correct transformation method to create a DataSet
+     * and a TransformationResult with the transformation errors
+     * @param data the raw data String base64 encoded
+     * @return TransformationResult
+     */
+    TransformationResult read(Base64 data);
+
+    /**
+     * Receives byte data from frontend, converts it to
+     * the corresponding filetype and tries to estimate
+     * the datatypes of each column. The result will be
+     * returned in a partial DataConfiguration object
+     * where only the DataTypes are specified
+     * @param data the raw data String base64 encoded
+     * @return DataConfiguration, only DataConfiguration populated
+     */
+    DataConfiguration estimateDatatypes(Base64 data);
+
 }

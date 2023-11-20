b46726a87087ecf5f3819bf100d4f43b75e0a785
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2023-11-20 13:37:10 +0100

    Added implementation for DataschemeGenerator

--- src/main/java/de/kiaim/platform/helper/DataschemeGenerator.java
+++ src/main/java/de/kiaim/platform/helper/DataschemeGenerator.java
@@ -1,4 +1,53 @@
 package de.kiaim.platform.helper;
 
+import de.kiaim.platform.model.DataConfiguration;
+import de.kiaim.platform.model.data.DataType;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DataschemeGenerator {
+
+	public String createSchema(final DataConfiguration dataConfiguration, final long id) {
+
+		String query = crateTableString(id);
+		StringBuilder columns = new StringBuilder();
+
+		for (final DataType dataType : dataConfiguration.getDataTypes()) {
+			switch (dataType) {
+				case BOOLEAN -> {
+					columns.append(createColumnString("", "boolean"));
+				}
+				case DATE_TIME -> {
+					columns.append(createColumnString("", "timestamp"));
+				}
+				case DECIMAL -> {
+					columns.append(createColumnString("", "numeric"));
+				}
+				case INTEGER -> {
+					columns.append(createColumnString("", "integer"));
+				}
+				case STRING -> {
+					columns.append(createColumnString("", "character varying"));
+				}
+				case DATE -> {
+					columns.append(createColumnString("", "date"));
+				}
+			}
+
+		}
+
+		return query.formatted(columns.toString());
+	}
+
+	private String createColumnString(final String columnName, final String dataType) {
+		return columnName + " " + dataType + " NOT NULL,";
+	}
+
+	private String crateTableString(final long id) {
+		return "CREATE TABLE data_set_" + id +
+		       "(" +
+		       "%s" +
+		       ");";
+	}
+
 }
---
+++ src/test/java/de/kiaim/platform/helper/DataschemeGeneratorTest.java
@@ -0,0 +1,43 @@
+package de.kiaim.platform.helper;
+
+import de.kiaim.platform.model.DataConfiguration;
+import de.kiaim.platform.model.data.DataType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataschemeGeneratorTest {
+
+	private static DataschemeGenerator dataschemeGenerator;
+
+	@Test
+	void createSchema() {
+		final List<DataType> dataTypes = List.of(DataType.BOOLEAN,
+		                                         DataType.DATE,
+		                                         DataType.DATE_TIME,
+		                                         DataType.DECIMAL,
+		                                         DataType.INTEGER,
+		                                         DataType.STRING);
+		final DataConfiguration dataConfiguration = new DataConfiguration(dataTypes);
+		final String query = dataschemeGenerator.createSchema(dataConfiguration, 1);
+
+		final String expected = "CREATE TABLE data_set_1(" +
+		                        " boolean NOT NULL," +
+		                        " date NOT NULL," +
+		                        " timestamp NOT NULL," +
+		                        " numeric NOT NULL," +
+		                        " integer NOT NULL," +
+		                        " character varying NOT NULL," +
+		                        ");";
+
+		assertEquals(expected, query);
+	}
+
+	@BeforeAll
+	static void beforeAll() {
+		dataschemeGenerator = new DataschemeGenerator();
+	}
+}

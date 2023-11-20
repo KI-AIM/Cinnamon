6ef34fe322b20b81e6049b8e84c9abf5d6274c64
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-11-20 17:22:28 +0100

    Adjusted CommonDataProcessor to use new configuration and include validation

--- src/main/java/de/kiaim/platform/processor/CommonDataProcessor.java
+++ src/main/java/de/kiaim/platform/processor/CommonDataProcessor.java
@@ -1,10 +1,12 @@
 package de.kiaim.platform.processor;
 
-import de.kiaim.platform.helper.DataHelper;
+import de.kiaim.platform.helper.DataTransformationHelper;
+import de.kiaim.platform.helper.ExceptionToTransformationErrorMapper;
 import de.kiaim.platform.model.*;
 import de.kiaim.platform.model.data.Data;
 import de.kiaim.platform.model.data.DataRow;
-import de.kiaim.platform.model.data.DataType;
+import de.kiaim.platform.model.data.configuration.ColumnConfiguration;
+import de.kiaim.platform.model.data.configuration.DataConfiguration;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -45,7 +47,8 @@ public abstract class CommonDataProcessor implements DataProcessor {
             String data,
             DataConfiguration config
     ) {
-        DataHelper dataHelper = new DataHelper();
+        DataTransformationHelper dataHelper = new DataTransformationHelper();
+        ExceptionToTransformationErrorMapper exceptionToTransformationErrorMapper = new ExceptionToTransformationErrorMapper();
 
         //Create objects to store results
         List<DataRow> dataRows = new ArrayList<>();
@@ -66,17 +69,26 @@ public abstract class CommonDataProcessor implements DataProcessor {
             for (int colIndex = 0; colIndex < cols.size(); colIndex++) {
                 // Process every column in a row
                 String col = cols.get(colIndex);
-                DataType type = config.getDataTypes().get(colIndex);
+                int finalColIndex = colIndex; //Variable in lambda expression must be effectively final
+
+                List<ColumnConfiguration> matchingColumnConfigurations = config.getConfigurations()
+                        .stream()
+                        .filter(it -> it.getIndex() == finalColIndex)
+                        .toList();
+
+                // Every index should appear exactly once
+                assert !matchingColumnConfigurations.isEmpty();
+                ColumnConfiguration columnConfiguration = matchingColumnConfigurations.get(0);
 
                 try {
-                    Data transformedData = dataHelper.transformData(col, type, config);
+                    Data transformedData = dataHelper.transformData(col, columnConfiguration);
                     transformedCol.add(transformedData);
                 } catch (Exception e) {
                     // Transformation error that was thrown inside the Data builders
                     DataRowTransformationError newError = new DataRowTransformationError(rowIndex, cols);
 
-                    //TODO: Add method to resolve TransformationErrorType from Exception and add them to the DataRowTransformationError
-                    newError.addError(new DataTransformationError(colIndex, TransformationErrorType.FORMAT_ERROR));
+                    TransformationErrorType errorType = exceptionToTransformationErrorMapper.mapException(e);
+                    newError.addError(new DataTransformationError(colIndex, errorType));
 
                     errors.add(newError);
                     errorInRow = true;

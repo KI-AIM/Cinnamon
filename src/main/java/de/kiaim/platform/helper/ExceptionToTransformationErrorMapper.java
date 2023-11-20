5a343c91084834eb6f948085a59d4c106267a8f8
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-11-20 17:22:49 +0100

    Implemented method that resolves custom exceptions to ErrorType

---
+++ src/main/java/de/kiaim/platform/helper/ExceptionToTransformationErrorMapper.java
@@ -0,0 +1,29 @@
+package de.kiaim.platform.helper;
+
+import de.kiaim.platform.model.TransformationErrorType;
+import de.kiaim.platform.model.data.configuration.DateTimeFormatConfiguration;
+import de.kiaim.platform.model.data.exception.*;
+
+public class ExceptionToTransformationErrorMapper {
+
+    public TransformationErrorType mapException(Exception e) {
+        if (e instanceof BooleanFormatException) {
+            return TransformationErrorType.FORMAT_ERROR;
+        } else if (e instanceof ConfigurationFormatException) {
+            return TransformationErrorType.CONFIG_ERROR;
+        } else if (e instanceof DateFormatException) {
+            return TransformationErrorType.FORMAT_ERROR;
+        } else if (e instanceof DateTimeFormatException) {
+            return TransformationErrorType.FORMAT_ERROR;
+        } else if (e instanceof FloatFormatException) {
+            return TransformationErrorType.FORMAT_ERROR;
+        } else if (e instanceof IntFormatException) {
+            return TransformationErrorType.FORMAT_ERROR;
+        } else if (e instanceof StringPatternException) {
+            return TransformationErrorType.FORMAT_ERROR;
+        } else {
+            return TransformationErrorType.OTHER;
+        }
+    }
+
+}

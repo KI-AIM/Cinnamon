1ba931241b12621d5cd856058758a94a47d7a160
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2023-11-28 13:52:32 +0100

    Implemented DataController

--- src/main/java/de/kiaim/platform/controller/DataController.java
+++ src/main/java/de/kiaim/platform/controller/DataController.java
@@ -1,18 +1,25 @@
 package de.kiaim.platform.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.kiaim.platform.model.TransformationResult;
 import de.kiaim.platform.model.data.configuration.DataConfiguration;
+import de.kiaim.platform.model.data.configuration.StringPatternConfiguration;
+import de.kiaim.platform.model.data.exception.BadFileException;
+import de.kiaim.platform.model.data.exception.DataSetPersistanceException;
 import de.kiaim.platform.processor.CsvProcessor;
+import de.kiaim.platform.processor.DataProcessor;
+import de.kiaim.platform.service.DatabaseService;
+import jakarta.annotation.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 // TODO Support different languages
 // TODO Error codes?
@@ -22,57 +29,144 @@ public class DataController {
 
 	// TODO Find Processor dynamically
 	private final CsvProcessor csvProcessor;
+	private final DatabaseService databaseService;
+	private final ObjectMapper objectMapper;
 
 	@Autowired
-	public DataController(final CsvProcessor csvProcessor) {
+	public DataController(final CsvProcessor csvProcessor, DatabaseService databaseService, ObjectMapper objectMapper) {
 		this.csvProcessor = csvProcessor;
+		this.databaseService = databaseService;
+		this.objectMapper = objectMapper;
 	}
 
 	@PostMapping(value = "/datatypes", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> estimateDatatpes(@RequestBody MultipartFile file) {
+	public ResponseEntity<Object> estimateDatatpes(@RequestBody MultipartFile file) {
+		return handleRequest(RequestType.DATA_TYPES, file, null, null);
+	}
+
+	@PostMapping(value = "/validation", produces = MediaType.APPLICATION_JSON_VALUE,
+	             consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Object> readAndValidateData(@RequestPart MultipartFile file,
+	                                                  @RequestParam String configuration) throws JsonProcessingException {
+		final DataConfiguration dataConfiguration = objectMapper.readValue(configuration, DataConfiguration.class);
+		return handleRequest(RequestType.VALIDATE, file, dataConfiguration, null);
+	}
+
+	@PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> storeData(@RequestPart MultipartFile file,
+	                                        @RequestParam String configuration) throws JsonProcessingException {
+		final DataConfiguration dataConfiguration = objectMapper.readValue(configuration, DataConfiguration.class);
+		return handleRequest(RequestType.STORE, file, dataConfiguration, null);
+	}
+
+	// TODO what to load
+	public void loadData() {
+	}
+
+	@DeleteMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> deleteData(@RequestParam Long dataSetId) {
+		return handleRequest(RequestType.DELETE, null, null , dataSetId);
+	}
+
+	/**
+	 * For each RequestTypes requires different attributes to be not null:
+	 * <ul>
+	 *     <li>{@link RequestType#DATA_TYPES}: file</li>
+	 *     <li>{@link RequestType#DELETE}: dataSetId</li>
+	 *     <li>{@link RequestType#STORE}: file, configuration</li>
+	 *     <li>{@link RequestType#VALIDATE}: file, configuration</li>
+	 * </ul>
+	 *
+	 *
+	 */
+	private ResponseEntity<Object> handleRequest(
+			final RequestType requestType,
+			@Nullable final MultipartFile file,
+			@Nullable final DataConfiguration configuration,
+			@Nullable final Long dataSetId
+	) {
+		try {
+			return doHandleRequest(requestType, file, configuration, dataSetId);
+		} catch (BadFileException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		} catch (DataSetPersistanceException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	private ResponseEntity<Object> doHandleRequest(
+			final RequestType requestType,
+			final MultipartFile file,
+			final DataConfiguration configuration,
+			final Long dataSetId
+	) throws BadFileException, DataSetPersistanceException {
+		final DataProcessor dataProcessor = getDataProcessor(file);
+
+		final InputStream inputStream;
+		try {
+			inputStream = file.getInputStream();
+		} catch (IOException e) {
+			throw new BadFileException("Could not read file");
+		}
+
+		final Object result;
+		switch (requestType) {
+			case DATA_TYPES -> {
+				result = dataProcessor.estimateDatatypes(inputStream);
+			}
+			case DELETE -> {
+				databaseService.delete(dataSetId);
+				result = null;
+			}
+			case STORE -> {
+				final TransformationResult transformationResult = dataProcessor.read(inputStream, configuration);
+				result = databaseService.store(transformationResult.getDataSet());
+			}
+			case VALIDATE -> {
+				result = dataProcessor.read(inputStream, configuration);
+			}
+			default -> {
+				return new ResponseEntity<>("Missing handling for request type '" + requestType.name() + "'",
+				                            HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	private DataProcessor getDataProcessor(final MultipartFile file) throws BadFileException {
+		final String fileExtension = extractFileExtension(file);
+
+		switch (fileExtension) {
+			case ".csv":
+				return csvProcessor;
+			default:
+				throw new BadFileException("Unsupported file type: '" + fileExtension + "'");
+		}
+	}
+
+	private String extractFileExtension(final MultipartFile file) throws BadFileException {
 		if (file == null) {
-			return new ResponseEntity<>("Missing file", HttpStatus.BAD_REQUEST);
+			throw new BadFileException("Missing file");
 		}
 
 		final String fileName = file.getOriginalFilename();
 		if (fileName == null || fileName.isBlank()) {
-			return new ResponseEntity<>("Missing filename", HttpStatus.BAD_REQUEST);
+			throw new BadFileException("Missing filename");
 		}
 
 		final int fileExtensionBegin = file.getOriginalFilename().lastIndexOf('.');
 		if (fileExtensionBegin == -1) {
-			return new ResponseEntity<>("Missing file extension", HttpStatus.BAD_REQUEST);
+			throw new BadFileException("Missing file extension");
 		}
 
-		final String fileExtension = file.getOriginalFilename().substring(fileExtensionBegin);
-
-		switch (fileExtension) {
-			case ".csv":
-				try {
-					final DataConfiguration dataConfiguration =  csvProcessor.estimateDatatypes(file.getInputStream());
-					return new ResponseEntity<>(dataConfiguration, HttpStatus.OK);
-				} catch (IOException e) {
-					return new ResponseEntity<>("Could not read file", HttpStatus.BAD_REQUEST);
-				}
-			default:
-				return new ResponseEntity<>("Unsupported file type: '" + fileExtension + "'", HttpStatus.BAD_REQUEST);
-		}
-    }
-
-	public void readAndValidateData() {
-
+		return file.getOriginalFilename().substring(fileExtensionBegin);
 	}
 
-	public void storeData() {
-
-	}
-
-	public void loadData() {
-
-	}
-
-	public void deleteData() {
-
+	private enum RequestType {
+		DATA_TYPES,
+		DELETE,
+		STORE,
+		VALIDATE;
 	}
 
 }
---
+++ src/main/java/de/kiaim/platform/model/data/exception/BadFileException.java
@@ -0,0 +1,7 @@
+package de.kiaim.platform.model.data.exception;
+
+public class BadFileException extends Exception {
+	public BadFileException(final String message) {
+		super(message);
+	}
+}

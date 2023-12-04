7c65fd3b37255b2955802866ec34c6be8eb48211
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-12-04 11:09:43 +0100

    Added Step configuration as json object

--- ki-aim-frontend/src/app/core/enums/steps.ts
+++ ki-aim-frontend/src/app/core/enums/steps.ts
@@ -3,5 +3,13 @@ export enum Steps {
     UPLOAD,
     DATA_CONFIG,
     VALIDATION,
-    PERSISTING,
+    PERSISTING
+}
+
+export const StepConfiguration = {
+    WELCOME: {"path": "/start", "id": "navLinkStart", "text": "Welcome", "enum": Steps.WELCOME, "dependsOn": "", "index": 0},
+    UPLOAD: {"path": "/upload", "id": "navLinkUpload", "text": "Upload data", "enum": Steps.UPLOAD, "dependsOn": Steps.WELCOME,  "index": 1},
+    DATA_CONFIG: {"path": "/dataConfiguration", "id": "navLinkDataConfiguration", "text": "Data configuration", "enum": Steps.DATA_CONFIG, "dependsOn": Steps.UPLOAD,  "index": 2},
+    //VALIDATION: {"path": "", "id": "", "text": "", "enum": Steps.VALIDATION, "index": 3},
+    //PERSISTING: {"path": "", "id": "", "text": "", "enum": Steps.PERSISTING, "index": 4},
 }

ed1206845446ca6693c489af52b80b796d47f09a
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-12-04 11:12:44 +0100

    Added data configuration service that stores the current configuration

---
+++ ki-aim-frontend/src/app/shared/services/data-configuration.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DataConfigurationService } from './data-configuration.service';
+
+describe('DataConfigurationService', () => {
+  let service: DataConfigurationService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(DataConfigurationService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});

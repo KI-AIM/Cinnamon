17101d43359814b3a935d7b4f242037183ce73f6
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-12-04 11:11:50 +0100

    Added modules

---
+++ ki-aim-frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ColumnConfiguration } from './model/column-configuration';
+import { DataConfigurationService } from './services/data-configuration.service';
+
+@NgModule({
+    declarations: [
+    ],
+    imports: [CommonModule],
+    exports: [],
+})
+export class SharedModule {}

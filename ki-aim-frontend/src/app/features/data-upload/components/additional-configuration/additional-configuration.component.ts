c3cf5e0432443b138bf6f44c47dff50caec79e3d
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-12-05 16:49:27 +0100

    Adjusted additional configuration component to show the correct configuration types

--- ki-aim-frontend/src/app/features/data-upload/components/additional-configuration/additional-configuration.component.ts
+++ ki-aim-frontend/src/app/features/data-upload/components/additional-configuration/additional-configuration.component.ts
@@ -1,21 +1,100 @@
-import { Component, Input } from '@angular/core';
-import { List } from 'src/app/core/utils/list';
-import { ConfigurationTypes, DataTypeToConfigurationTypeMapping } from 'src/app/shared/model/configuration-types';
-import { DataType } from 'src/app/shared/model/data-type';
+import { Component, ElementRef, Input, ViewChild } from "@angular/core";
+import {
+	ConfigurationType,
+	getConfigurationForConfigurationType,
+	getConfigurationTypeForString,
+	getConfigurationTypeStringForIndex,
+	getConfigurationsForDatatype,
+} from "src/app/shared/model/configuration-types";
+import { ColumnConfiguration } from "src/app/shared/model/column-configuration";
+import { List } from "src/app/core/utils/list";
+import { DateformatComponent } from "../configurationSettings/dateformat/dateformat.component";
+import { DatetimeformatComponent } from "../configurationSettings/datetimeformat/datetimeformat.component";
+import { StringpatternComponent } from "../configurationSettings/stringpattern/stringpattern.component";
 
 @Component({
-    selector: 'app-additional-configuration',
-    templateUrl: './additional-configuration.component.html',
-    styleUrls: ['./additional-configuration.component.less'],
+	selector: "app-additional-configuration",
+	templateUrl: "./additional-configuration.component.html",
+	styleUrls: ["./additional-configuration.component.less"],
 })
 export class AdditionalConfigurationComponent {
-    @Input() attrNumber: Number; 
+	ConfigurationType = ConfigurationType;
 
-    getConfigurationsForDatatype(type: DataType) {
-        var result = new List<ConfigurationTypes>(); 
+	@Input() attrNumber: Number;
+	@Input() column: ColumnConfiguration;
 
-        var typeIndex = Object.keys(DataType).indexOf(type.toString());
+	@ViewChild("dateFormat") dateFormatComponent: DateformatComponent;
+	@ViewChild("dateTimeFormat")
+	dateTimeFormatComponent: DatetimeformatComponent;
+	@ViewChild("stringPattern") stringPatternComponent: StringpatternComponent;
 
-        var mappedConfigurationsTypes = DataTypeToConfigurationTypeMapping
+	currentConfigurationSelection: ConfigurationType | undefined;
+
+	changeConfigurationSelection(event: any) {
+		this.currentConfigurationSelection = event.target.value;
+	}
+
+    areConfigurationAvailable(type: String): boolean {
+        return this.getConfigurationsForDatatype(type).size() > 0;  
     }
+
+	getConfigurationsForDatatype(type: String): List<ConfigurationType> {
+        var result = new List<ConfigurationType>
+        var configurationTypes = getConfigurationsForDatatype(type); 
+
+        configurationTypes.getAll().forEach(configurationType => {
+            if (!this.isConfigurationAlreadyAdded(configurationType)) {
+                result.add(configurationType); 
+            }
+        });
+
+		return result;
+	}
+
+    isConfigurationAlreadyAdded(configurationType: ConfigurationType): boolean {
+        var result = false; 
+        this.column.configurations.forEach(configuration => {
+            if (configuration.constructor.name == getConfigurationForConfigurationType(configurationType)) {
+                result = true; 
+            }
+        });
+
+        return result; 
+    }
+
+	getConfigurationTypeForIndex(index: number): String {
+		return getConfigurationTypeStringForIndex(index);
+	}
+
+	addConfiguration() {
+        if (this.currentConfigurationSelection != undefined) {
+            switch (
+                getConfigurationTypeForString(
+                    ConfigurationType[this.currentConfigurationSelection]
+                )
+            ) {
+                case ConfigurationType.DATEFORMAT: {
+                    this.column.addConfiguration(
+                        this.dateFormatComponent.getDateFormatConfiguration()
+                    );
+                    this.currentConfigurationSelection = undefined;
+                    break;
+                }
+                case ConfigurationType.DATETIMEFORMAT: {
+                    this.column.addConfiguration(
+                        this.dateTimeFormatComponent.getDateTimeFormatConfiguration()
+                    );
+                    this.currentConfigurationSelection = undefined;
+                    break;
+                }
+                case ConfigurationType.STRINGPATTERN: {
+                    this.column.addConfiguration(
+                        this.stringPatternComponent.getStringPatternConfiguration()
+                    );
+                    this.currentConfigurationSelection = undefined;
+                    break;
+                }
+            }
+        }
+	}
 }

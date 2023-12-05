60a7139453ffd68a4a7cf1f545b1650300fda0f8
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-12-05 16:50:45 +0100

    Added string transformation method for data types

--- ki-aim-frontend/src/app/shared/model/data-type.ts
+++ ki-aim-frontend/src/app/shared/model/data-type.ts
@@ -7,3 +7,15 @@ export enum DataType {
 	DATE,
 	UNDEFINED
 }
+
+export function dataTypeFromString(value: String): DataType {
+	switch (value) {
+		case "BOOLEAN": return DataType.BOOLEAN
+		case "DATE_TIME": return DataType.DATE_TIME
+		case "DECIMAL": return DataType.DECIMAL
+		case "INTEGER": return DataType.INTEGER
+		case "STRING": return DataType.STRING
+		case "DATE": return DataType.DATE
+		default: return DataType.UNDEFINED
+	}
+}

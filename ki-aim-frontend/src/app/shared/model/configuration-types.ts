1d870211101df7ebbe0e9d79883b4c7c605091f7
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-12-05 16:50:33 +0100

    Adjusted configuration type to be usable as values

--- ki-aim-frontend/src/app/shared/model/configuration-types.ts
+++ ki-aim-frontend/src/app/shared/model/configuration-types.ts
@@ -1,34 +1,60 @@
-import { DataType } from "./data-type"
-import { DateFormatConfiguration } from "./date-format-configuration"
-import { DateTimeFormatConfiguration } from "./date-time-format-configuration"
-import { StringPatternConfiguration } from "./string-pattern-configuration"
+import { List } from "src/app/core/utils/list";
+import { DataType } from "./data-type";
+import { DateFormatConfiguration } from "./date-format-configuration";
+import { DateTimeFormatConfiguration } from "./date-time-format-configuration";
+import { StringPatternConfiguration } from "./string-pattern-configuration";
+import { Configuration } from "./configuration";
+import { dataTypeFromString } from "./data-type";
+import { Type } from "@angular/core";
 
-export enum ConfigurationTypes {
-    DATEFORMAT,
-    DATETIMEFORMAT,
-    STRINGPATTERN
+export enum ConfigurationType {
+	DATEFORMAT,
+	DATETIMEFORMAT,
+	STRINGPATTERN,
 }
 
-export const DataTypeToConfigurationTypeMapping = {
-    [DataType.DATE] : [
-        ConfigurationTypes.DATEFORMAT,
-    ],
-    [DataType.DATE_TIME]: [
-        ConfigurationTypes.DATETIMEFORMAT
-    ],
-    [DataType.STRING]: [
-        ConfigurationTypes.STRINGPATTERN
-    ]
+export function getConfigurationTypeForString(value: String): ConfigurationType | undefined {
+    switch (value){
+        case "DATEFORMAT": return ConfigurationType.DATEFORMAT
+        case "DATETIMEFORMAT": return ConfigurationType.DATETIMEFORMAT
+        case "STRINGPATTERN": return ConfigurationType.STRINGPATTERN
+        default: return undefined; 
+    }
 }
 
-export const DataTypeToConfigurationClassMapping = {
-    [DataType.DATE] : [
-        DateFormatConfiguration
-    ],
-    [DataType.DATE_TIME]: [
-        DateTimeFormatConfiguration
-    ],
-    [DataType.STRING]: [
-        StringPatternConfiguration
-    ]
+export function getConfigurationTypeStringForIndex(index: number) {
+    return ConfigurationType[index];
+}
+
+
+/**
+ * Obejcts need to be complete
+ * in order to perform indexing by type
+ */
+export const DataTypeToConfigurationTypeMapping: Record<
+	DataType,
+	ConfigurationType[]
+> = {
+	[DataType.BOOLEAN]: [],
+	[DataType.DECIMAL]: [],
+	[DataType.INTEGER]: [],
+	[DataType.DATE]: [ConfigurationType.DATEFORMAT],
+	[DataType.DATE_TIME]: [ConfigurationType.DATETIMEFORMAT],
+	[DataType.STRING]: [ConfigurationType.STRINGPATTERN],
+	[DataType.UNDEFINED]: [],
+};
+
+export function getConfigurationsForDatatype(
+	type: String
+): List<ConfigurationType> {
+	var datatype = dataTypeFromString(type);
+	return new List(DataTypeToConfigurationTypeMapping[datatype] || []);
+}
+
+export function getConfigurationForConfigurationType(type: ConfigurationType): String{
+    switch (type) {
+        case ConfigurationType.DATEFORMAT: return DateFormatConfiguration.name; 
+        case ConfigurationType.DATETIMEFORMAT: return DateTimeFormatConfiguration.name; 
+        case ConfigurationType.STRINGPATTERN: return StringPatternConfiguration.name; 
+    }
 }

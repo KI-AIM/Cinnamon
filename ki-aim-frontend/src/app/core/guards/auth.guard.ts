d1d8d6efced3018d638517667abc15e2539778e2
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-12-12 13:56:23 +0100

    Basic code formatting

--- ki-aim-frontend/src/app/core/guards/auth.guard.ts
+++ ki-aim-frontend/src/app/core/guards/auth.guard.ts
@@ -1,28 +1,35 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import { UserService } from 'src/app/shared/services/user.service';
+import { Injectable } from "@angular/core";
+import {
+	ActivatedRouteSnapshot,
+	CanActivate,
+	Router,
+	RouterStateSnapshot,
+	UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { UserService } from "src/app/shared/services/user.service";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
+	constructor(
+		private readonly userService: UserService,
+		private readonly router: Router
+	) {}
 
-
-  constructor(
-    private readonly userService: UserService,
-    private readonly router: Router,
-  ) {}
-  
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    if (!this.userService.isAuthenticated()) {
-      this.router.navigate(['login', {mode: 'noPermission'}]);
-      return false;
-    }
-    return true;
-  }
-  
+	canActivate(
+		route: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot
+	):
+		| Observable<boolean | UrlTree>
+		| Promise<boolean | UrlTree>
+		| boolean
+		| UrlTree {
+		if (!this.userService.isAuthenticated()) {
+			this.router.navigate(["login", { mode: "noPermission" }]);
+			return false;
+		}
+		return true;
+	}
 }

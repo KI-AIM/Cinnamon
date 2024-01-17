2163738492aa4e56c8fb3836f7f0cad8c952572a
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2024-01-17 14:18:08 +0100

    Added test coverage for all types of RangeConfigurations

--- src/test/java/de/kiaim/platform/TestModelHelper.java
+++ src/test/java/de/kiaim/platform/TestModelHelper.java
@@ -6,9 +6,7 @@ import de.kiaim.platform.model.data.configuration.*;
 import de.kiaim.platform.model.file.CsvFileConfiguration;
 import de.kiaim.platform.model.file.FileConfiguration;
 import de.kiaim.platform.model.file.FileType;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.mock.web.MockMultipartFile;
-import org.w3c.dom.ranges.Range;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -26,28 +24,24 @@ public class TestModelHelper {
 	}
 
 	public static DataConfiguration generateDataConfiguration(final String stringPattern) {
-		final DataConfiguration dataConfiguration = new DataConfiguration();
+		final DataConfiguration dataConfiguration = generateEstimatedConfiguration();
+
+		final DateFormatConfiguration dateFormatConfiguration = new DateFormatConfiguration("yyyy-MM-dd");
+		dataConfiguration.getConfigurations().get(1).addConfiguration(dateFormatConfiguration);
+		final RangeConfiguration dateRangeConfiguration = new RangeConfiguration(new DateData(LocalDate.of(1970, 1, 1)), new DateData(LocalDate.of(2030, 1, 1)));
+		dataConfiguration.getConfigurations().get(1).addConfiguration(dateRangeConfiguration);
+
+		final DateTimeFormatConfiguration dateTimeFormatConfiguration = new DateTimeFormatConfiguration("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+		dataConfiguration.getConfigurations().get(2).addConfiguration(dateTimeFormatConfiguration);
+		final RangeConfiguration dateTimeRangeConfiguration = new RangeConfiguration(new DateTimeData(LocalDateTime.of(1970, 1, 1, 0, 1)), new DateTimeData(LocalDateTime.of(2030, 1, 1, 23, 59)));
+		dataConfiguration.getConfigurations().get(2).addConfiguration(dateTimeRangeConfiguration);
+
+		final RangeConfiguration integerRangeConfiguration = new RangeConfiguration(new IntegerData(0), new IntegerData(100));
+		dataConfiguration.getConfigurations().get(4).addConfiguration(integerRangeConfiguration);
 
 		final StringPatternConfiguration stringPatternConfiguration = new StringPatternConfiguration(stringPattern);
-		final DateFormatConfiguration dateFormatConfiguration = new DateFormatConfiguration("yyyy-MM-dd");
-		final DateTimeFormatConfiguration dateTimeFormatConfiguration = new DateTimeFormatConfiguration("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
-		final RangeConfiguration integerRangeConfiguration = new RangeConfiguration(new IntegerData(0), new IntegerData(100));
+		dataConfiguration.getConfigurations().get(5).addConfiguration(stringPatternConfiguration);
 
-		final List<ColumnConfiguration> columnConfigurations = List.of(
-				new ColumnConfiguration(0, "column0_boolean", DataType.BOOLEAN, DataScale.NOMINAL ,new ArrayList<>()),
-				new ColumnConfiguration(1, "column1_date", DataType.DATE, DataScale.INTERVAL, List.of(dateFormatConfiguration)),
-				new ColumnConfiguration(2, "column2_date_time", DataType.DATE_TIME, DataScale.INTERVAL, List.of(dateTimeFormatConfiguration)),
-				new ColumnConfiguration(3, "column3_decimal", DataType.DECIMAL, DataScale.RATIO, new ArrayList<>()),
-				new ColumnConfiguration(4, "column4_integer", DataType.INTEGER, DataScale.INTERVAL, List.of(integerRangeConfiguration)),
-				new ColumnConfiguration(5, "column5_string", DataType.STRING, DataScale.NOMINAL, List.of(stringPatternConfiguration)));
-		dataConfiguration.setConfigurations(columnConfigurations);
-		final List<DataType> dataTypes = List.of(DataType.BOOLEAN,
-		                                         DataType.DATE,
-		                                         DataType.DATE_TIME,
-		                                         DataType.DECIMAL,
-		                                         DataType.INTEGER,
-		                                         DataType.STRING);
-		dataConfiguration.setDataTypes(dataTypes);
 		return dataConfiguration;
 	}
 
@@ -124,7 +118,7 @@ public class TestModelHelper {
 
 	public static String generateDataConfigurationAsJson() {
 		return """
-				{"dataTypes":["BOOLEAN","DATE","DATE_TIME","DECIMAL","INTEGER","STRING"],"configurations":[{"index":0,"name":"column0_boolean","type":"BOOLEAN","scale":"NOMINAL","configurations":[]},{"index":1,"name":"column1_date","type":"DATE","scale":"INTERVAL","configurations":[{"name":"DateFormatConfiguration","dateFormatter":"yyyy-MM-dd"}]},{"index":2,"name":"column2_date_time","type":"DATE_TIME","scale":"INTERVAL","configurations":[{"name":"DateTimeFormatConfiguration","dateTimeFormatter":"yyyy-MM-dd'T'HH:mm:ss.SSSSSS"}]},{"index":3,"name":"column3_decimal","type":"DECIMAL","scale":"RATIO","configurations":[]},{"index":4,"name":"column4_integer","type":"INTEGER","scale":"INTERVAL","configurations":[{"name":"RangeConfiguration","minValue":0,"maxValue":100}]},{"index":5,"name":"column5_string","type":"STRING","scale":"NOMINAL","configurations":[{"name":"StringPatternConfiguration","pattern":".*"}]}]}""";
+				{"dataTypes":["BOOLEAN","DATE","DATE_TIME","DECIMAL","INTEGER","STRING"],"configurations":[{"index":0,"name":"column0_boolean","type":"BOOLEAN","scale":"NOMINAL","configurations":[]},{"index":1,"name":"column1_date","type":"DATE","scale":"INTERVAL","configurations":[{"name":"DateFormatConfiguration","dateFormatter":"yyyy-MM-dd"},{"name":"RangeConfiguration","minValue":"1970-01-01","maxValue":"2030-01-01"}]},{"index":2,"name":"column2_date_time","type":"DATE_TIME","scale":"INTERVAL","configurations":[{"name":"DateTimeFormatConfiguration","dateTimeFormatter":"yyyy-MM-dd'T'HH:mm:ss.SSSSSS"},{"name":"RangeConfiguration","minValue":"1970-01-01T00:01:00","maxValue":"2030-01-01T23:59:00"}]},{"index":3,"name":"column3_decimal","type":"DECIMAL","scale":"RATIO","configurations":[]},{"index":4,"name":"column4_integer","type":"INTEGER","scale":"INTERVAL","configurations":[{"name":"RangeConfiguration","minValue":0,"maxValue":100}]},{"index":5,"name":"column5_string","type":"STRING","scale":"NOMINAL","configurations":[{"name":"StringPatternConfiguration","pattern":".*"}]}]}""";
 	}
 
 	public static MockMultipartFile loadCsvFile() throws IOException {

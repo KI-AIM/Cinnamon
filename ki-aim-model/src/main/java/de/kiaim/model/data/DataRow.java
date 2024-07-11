c9fdeceeed5924461549c02f727a230d9178366d
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2024-07-11 17:08:27 +0200

    Added function to convert a DataRow to a list

--- ki-aim-model/src/main/java/de/kiaim/model/data/DataRow.java
+++ ki-aim-model/src/main/java/de/kiaim/model/data/DataRow.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.ToString;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ArraySchema(schema = @Schema(implementation = Data.class,
                               example = "[true, \"2023-12-24\", \"2023-12-24T18:30:01.123456\", 4.2, 42, \"Hello World!\"]"))
@@ -18,4 +19,8 @@ import java.util.List;
 public class DataRow {
 
 	private final List<Data> data;
+
+	public final List<Object> getRow() {
+		return data.stream().map(Data::getValue).collect(Collectors.toList());
+	}
 }
--- ki-aim-model/src/main/java/de/kiaim/model/data/DataSet.java
+++ ki-aim-model/src/main/java/de/kiaim/model/data/DataSet.java
@@ -30,12 +30,7 @@ public class DataSet {
 
 	@ArraySchema(schema = @Schema(description = "Row of the data set.", implementation = DataRow.class))
 	public final List<List<Object>> getData() {
-		return dataRows.stream()
-		               .map(dataRow -> dataRow.getData()
-		                                      .stream()
-		                                      .map(Data::getValue)
-		                                      .collect(Collectors.toList()))
-		               .toList();
+		return dataRows.stream().map(DataRow::getRow).toList();
 	}
 
 	public void setData(final List<List<Data>> data) {

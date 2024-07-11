768176d916eaa6240b2cd2d3af299f08ff1977bd
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2024-07-11 17:08:43 +0200

    Removed unused imports

--- ki-aim-platform/src/main/java/de/kiaim/platform/config/WebConfig.java
+++ ki-aim-platform/src/main/java/de/kiaim/platform/config/WebConfig.java
@@ -2,8 +2,6 @@ package de.kiaim.platform.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;

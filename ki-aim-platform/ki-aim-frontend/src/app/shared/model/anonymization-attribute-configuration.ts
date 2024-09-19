f144c4a43c342319d3d9449e5d834feb138a9347
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2024-09-19 10:58:57 +0200

    Added model to store attribute configuration

---
+++ ki-aim-platform/ki-aim-frontend/src/app/shared/model/anonymization-attribute-configuration.ts
@@ -0,0 +1,5 @@
+import { AnonymizationAttributeRowConfiguration } from "./anonymization-attribute-row-configuration";
+
+export class AnonymizationAttributeConfiguration {
+    attributeConfiguration: AnonymizationAttributeRowConfiguration[]; 
+}

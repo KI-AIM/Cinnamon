c2e83fdf90674ca8d7758cae7093958c3495ae29
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2024-10-01 13:31:42 +0200

    Added anon attribute directive to handle events

--- ki-aim-platform/ki-aim-frontend/src/app/features/anonymization/directives/anonymization-attribute-configuration-directive.directive.spec.ts
+++ ki-aim-platform/ki-aim-frontend/src/app/features/anonymization/directives/anonymization-attribute-configuration-directive.directive.spec.ts
@@ -1,11 +1,12 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { AnonymizationAttributeConfigurationDirectiveDirective } from './anonymization-attribute-configuration-directive.directive';
+import { AnonymizationAttributeConfigurationDirective } from './anonymization-attribute-configuration.directive';
+import { ViewContainerRef } from '@angular/core';
 
 describe('Directive: AnonymizationAttributeConfigurationDirective', () => {
   it('should create an instance', () => {
-    const directive = new AnonymizationAttributeConfigurationDirectiveDirective();
+    const directive = new AnonymizationAttributeConfigurationDirective();
     expect(directive).toBeTruthy();
   });
 });

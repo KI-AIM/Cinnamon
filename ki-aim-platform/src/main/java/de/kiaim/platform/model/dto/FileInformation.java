d7697afa72091b6d417190d091c3b8ad19d7cae3
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2024-10-25 15:33:55 +0200

    Added nullable annotations

--- ki-aim-platform/src/main/java/de/kiaim/platform/model/dto/FileInformation.java
+++ ki-aim-platform/src/main/java/de/kiaim/platform/model/dto/FileInformation.java
@@ -6,17 +6,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 @Schema(description = "General information about a file.")
 @NoArgsConstructor @AllArgsConstructor
 @Getter @Setter
 public class FileInformation {
 	@Schema(description = "Name of the file.", example = "data.csv")
+	@Nullable
 	private String name;
 
 	@Schema(description = "File type of the file.", example = "CSV")
+	@Nullable
 	private FileType type;
 
 	@Schema(description = "Number of attributes in the file.", example = "CSV")
-	private int numberOfAttributes;
+	@Nullable
+	private Integer numberOfAttributes;
 }

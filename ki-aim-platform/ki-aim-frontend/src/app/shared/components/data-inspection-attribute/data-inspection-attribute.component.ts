ccf6fc4301b19687cdd72e15ad1cc315b1a6be75
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2024-10-28 12:21:28 +0100

    Added some styling

--- ki-aim-platform/ki-aim-frontend/src/app/shared/components/data-inspection-attribute/data-inspection-attribute.component.ts
+++ ki-aim-platform/ki-aim-frontend/src/app/shared/components/data-inspection-attribute/data-inspection-attribute.component.ts
@@ -1,11 +1,32 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ColumnConfiguration} from "../../model/column-configuration";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
     selector: 'app-data-inspection-attribute',
     templateUrl: './data-inspection-attribute.component.html',
     styleUrls: ['./data-inspection-attribute.component.less']
 })
-export class DataInspectionAttributeComponent {
+export class DataInspectionAttributeComponent implements OnInit {
     @Input() public configuration!: ColumnConfiguration;
+
+
+    protected catImage$: Observable<CatImageMetadata[]>;
+
+    constructor(
+        private readonly httpClient: HttpClient,
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.catImage$ = this.httpClient.get<CatImageMetadata[]>("https://api.thecatapi.com/v1/images/search");
+    }
+}
+
+class CatImageMetadata {
+    id: string;
+    url: string;
+    width: number;
+    height: number;
 }
--- ki-aim-platform/ki-aim-frontend/src/app/shared/components/data-inspection/data-inspection.component.ts
+++ ki-aim-platform/ki-aim-frontend/src/app/shared/components/data-inspection/data-inspection.component.ts
@@ -1,8 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable, tap } from "rxjs";
+import { Observable } from "rxjs";
 import { DataConfiguration } from "../../model/data-configuration";
 import { DataConfigurationService } from "../../services/data-configuration.service";
-import { HttpClient } from "@angular/common/http";
 
 @Component({
     selector: 'app-data-inspection',
@@ -13,31 +12,15 @@ export class DataInspectionComponent implements OnInit {
     @Input() public step!: string;
 
     protected dataConfiguration$: Observable<DataConfiguration>;
-    protected catImage$: Observable<CatImageMetadata[]>;
 
     protected filterText: string;
 
     constructor(
         private readonly dataConfigurationService: DataConfigurationService,
-        private readonly httpClient: HttpClient,
     ) {
     }
 
     ngOnInit(): void {
         this.dataConfiguration$ = this.dataConfigurationService.downloadDataConfiguration(this.step);
-        this.catImage$ = this.httpClient.get<CatImageMetadata[]>("https://api.thecatapi.com/v1/images/search");
     }
-
-
-    // protected getCatImage$(): Observable<CatImageMetadata> {
-    //     return this.httpClient.get<CatImageMetadata>("https://api.thecatapi.com/v1/images/search");
-    // }
-
-}
-
-class CatImageMetadata {
-    id: string;
-    url: string;
-    width: number;
-    height: number;
 }

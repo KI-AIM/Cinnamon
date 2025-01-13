8293d9e5ca5faabc7caca311a04fbb6fa8bbde72
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-01-13 09:48:16 +0100

    Fixed uninitialized metric settings

--- ki-aim-platform/ki-aim-frontend/src/app/shared/services/project-configuration.service.ts
+++ ki-aim-platform/ki-aim-frontend/src/app/shared/services/project-configuration.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { environments } from "../../../environments/environment";
-import { BehaviorSubject, Observable, shareReplay, tap } from "rxjs";
-import { ProjectSettings } from "../model/project-settings";
+import { BehaviorSubject, map, Observable, of, shareReplay, switchMap, tap } from "rxjs";
+import { MetricImportance, MetricSettings, ProjectSettings } from "../model/project-settings";
+import { TechnicalEvaluationService } from "../../features/technical-evaluation/services/technical-evaluation.service";
+import { ConfigurationGroupDefinition, ConfigurationGroupDefinitions } from "../model/configuration-group-definition";
 
 @Injectable({
     providedIn: 'root'
@@ -16,6 +18,7 @@ export class ProjectConfigurationService {
 
     constructor(
         private readonly http: HttpClient,
+        private readonly technicalEvaluationService: TechnicalEvaluationService,
     ) {
     }
 
@@ -23,6 +26,18 @@ export class ProjectConfigurationService {
         if (!this.projectSettingsSubject) {
             this.projectSettingsSubject = new BehaviorSubject<ProjectSettings>(new ProjectSettings());
             this._projectSettings$ = this.fetchProjectSettings().pipe(
+                switchMap(value => {
+                   if (value.metricConfiguration) {
+                       return of(value);
+                   } else {
+                       return this.initMetricSettings$().pipe(
+                           map(value1 => {
+                              value.metricConfiguration = value1;
+                              return value;
+                           }),
+                       );
+                   }
+                }),
                 tap(value => {
                     this.projectSettingsSubject?.next(value);
                 }),
@@ -47,6 +62,47 @@ export class ProjectConfigurationService {
 
     private putProjectSettings(projectSettings: ProjectSettings): Observable<void> {
         return this.http.put<void>(this.baseUrl + "/configuration", projectSettings);
+    }
+
+    /**
+     * Initializes the metric settings by fetching the definition of all metrics from the technical evaluation
+     * and setting each metric to {@link MetricImportance.IMPORTANT}.
+     *
+     * @return Observable containing the initialized metric settings.
+     * @private
+     */
+    private initMetricSettings$(): Observable<MetricSettings> {
+        return this.technicalEvaluationService.algorithms.pipe(
+            switchMap(value1 => {
+                return this.technicalEvaluationService.getAlgorithmDefinition(value1[0]);
+            }),
+            map(value1 => {
+                return this.initMetricSettings(value1.configurations);
+            }),
+        );
+    }
+
+    private initMetricSettings(configurations: ConfigurationGroupDefinitions): MetricSettings {
+        const metricSettings = new MetricSettings();
+        this.createGroups(metricSettings, configurations);
+        return metricSettings;
+    }
+
+    private createGroups(metricsSettings: MetricSettings, configurations: ConfigurationGroupDefinitions) {
+        Object.values(configurations).forEach((groupDefinition) => {
+            this.createGroup(metricsSettings, groupDefinition);
+        });
+    }
+
+    private createGroup(metricSettings: MetricSettings, groupDefinition: ConfigurationGroupDefinition): void {
+        if (groupDefinition.configurations) {
+            this.createGroups(metricSettings, groupDefinition.configurations);
+        }
+        if (groupDefinition.options) {
+            Object.keys(groupDefinition.options).forEach(inputDefinition => {
+                metricSettings[inputDefinition] = MetricImportance.IMPORTANT;
+            });
+        }
     }
 
 }

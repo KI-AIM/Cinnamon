6e20311c167a4b09b74bcaa77590906c4c5adc4b
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-03-13 14:28:17 +0100

    Added error if backend is not available

--- cinnamon-platform/cinnamon-frontend/src/app/shared/services/app-config.service.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/shared/services/app-config.service.ts
@@ -9,13 +9,20 @@ export class AppConfigService {
     private readonly baseURL = environments.apiUrl;
 
     public config: AppConfig | null = null;
+    public cinnamonAvailable: boolean = true;
 
     constructor(
         http: HttpClient,
     ) {
-        http.get<AppConfig>(this.baseURL + "/config.json").subscribe(config => {
-            this.config = config;
-        });
+        http.get<AppConfig>(this.baseURL + "/config.json").subscribe(
+            {
+                next: config => {
+                    this.config = config;
+                },
+                error: () => {
+                    this.cinnamonAvailable = false;
+                },
+            });
     }
 
 }

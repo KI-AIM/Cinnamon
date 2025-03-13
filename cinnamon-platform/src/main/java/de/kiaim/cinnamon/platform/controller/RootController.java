fd53075d251d52977a062dd8d934a51b035a4af7
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-03-13 16:06:16 +0100

    Moved endpoint for appConfig into root controller

--- cinnamon-platform/src/main/java/de/kiaim/cinnamon/platform/controller/RootController.java
+++ cinnamon-platform/src/main/java/de/kiaim/cinnamon/platform/controller/RootController.java
@@ -1,14 +1,33 @@
 package de.kiaim.cinnamon.platform.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class RootController {
+
+    @Value("${cinnamon.is-demo-instance}")
+    private boolean isDemoInstance;
 
     @RequestMapping(method = RequestMethod.GET, path = "/")
     public String index() {
         return "index.html";
     }
+
+    @ResponseBody
+    @GetMapping(value = "/config.json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> getConfig() {
+        Map<String, Object> config = new HashMap<>();
+        config.put("isDemoInstance", isDemoInstance);
+        return config;
+    }
+
 }

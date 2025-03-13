687bfa90bdcc6228277744b395841684a343ce0f
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-03-13 15:04:14 +0100

    Set cronjob schedule

--- cinnamon-platform/src/main/java/de/kiaim/cinnamon/platform/cronjob/ResetUsersCronjob.java
+++ cinnamon-platform/src/main/java/de/kiaim/cinnamon/platform/cronjob/ResetUsersCronjob.java
@@ -19,8 +19,7 @@ public class ResetUsersCronjob {
 		this.userService = userService;
 	}
 
-	//@Scheduled(cron = "0 0 2 * * ?")
-	@Scheduled(cron = "0 * * * * ?")
+	@Scheduled(cron = "0 0 2 * * ?")
 	public void resetUsers() {
 		log.info("Resetting users...");
 

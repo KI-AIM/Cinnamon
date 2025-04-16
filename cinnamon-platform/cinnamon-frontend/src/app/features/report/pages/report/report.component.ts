bd81d6d59a214624ce521a751d1bea5e2faa2d6e
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-04-16 17:06:08 +0200

    First attempt to generate a PDF

--- cinnamon-platform/cinnamon-frontend/src/app/features/report/pages/report/report.component.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/features/report/pages/report/report.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatButton } from "@angular/material/button";
 import { SharedModule } from "../../../../shared/shared.module";
 import { AsyncPipe, NgIf } from "@angular/common";
-import { catchError, filter, Observable, of, switchMap, take, timer } from "rxjs";
+import { Observable } from "rxjs";
 import { ProjectSettings } from "../../../../shared/model/project-settings";
 import { TitleService } from "../../../../core/services/title-service.service";
 import { ProjectConfigurationService } from "../../../../shared/services/project-configuration.service";
 import { StatisticsResponse } from "../../../../shared/model/statistics";
 import { StatisticsService } from "../../../../shared/services/statistics.service";
+import { ChartFrequencyComponent } from "../../../../shared/components/chart-frequency/chart-frequency.component";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
     selector: 'app-report',
@@ -25,7 +27,12 @@ export class ReportComponent implements OnInit {
     protected metricConfig$: Observable<ProjectSettings>;
     protected statistics$: Observable<StatisticsResponse | null>;
 
+
+    @ViewChild('chart', {read: ElementRef}) protected chartDiv: ElementRef<HTMLElement>;
+    @ViewChild('chart') protected chart: ChartFrequencyComponent;
+
     constructor(
+        private readonly http: HttpClient,
         private projectConfigService: ProjectConfigurationService,
         private statisticsService: StatisticsService,
         titleService: TitleService,
@@ -39,11 +46,14 @@ export class ReportComponent implements OnInit {
     }
 
     protected printReport(): void {
-        //@ts-ignore
-        // document.querySelector("#report").contentWindow?.print();
-        // window.print();
+        this.http.get("/app/assets/pdf.css", {responseType: 'text'}).subscribe({
+            next: value => {
+                this.doPrintReport(value);
+            },
+        });
+    }
 
-
+    private doPrintReport(styleString: string) {
         const mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
 
         if (!mywindow) {
@@ -51,15 +61,24 @@ export class ReportComponent implements OnInit {
         }
 
         mywindow.document.write(`<html><head><title>Report</title>`);
+        mywindow.document.write(`<style>${styleString}</style>`);
         mywindow.document.write('</head><body >');
         mywindow.document.write(document.getElementById("report")!.innerHTML);
         mywindow.document.write('</body></html>');
+
+        const image = document.createElement("img");
+        image.src = this.chart.dataUrl!;
+        image.width = 670;
+
+        const id = this.chartDiv.nativeElement.id;
+        const newChart = mywindow.document.getElementById(id);
+        newChart!.style.display = 'none';
+        newChart!.parentElement!.insertBefore(image, newChart);
 
         mywindow.document.close(); // necessary for IE >= 10
         mywindow.focus(); // necessary for IE >= 10*/
 
         mywindow.print();
-        // mywindow.close();
     }
 
 }

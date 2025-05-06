3e11d59a92ad1d5eeb2a62e4b8b80cc83b681ef6
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-05-06 11:07:06 +0200

    Use file upload component on the configuration page

--- cinnamon-platform/cinnamon-frontend/src/app/shared/components/configuration-upload/configuration-upload.component.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/shared/components/configuration-upload/configuration-upload.component.ts
@@ -1,9 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ConfigurationService } from 'src/app/shared/services/configuration.service';
 import { ImportPipeData } from "../../model/import-pipe-data";
-import { Observable } from "rxjs";
-import { StepConfiguration } from "../../model/step-configuration";
 import { ErrorHandlingService } from "../../services/error-handling.service";
 
 @Component({
@@ -12,114 +9,56 @@ import { ErrorHandlingService } from "../../services/error-handling.service";
     styleUrls: ['./configuration-upload.component.less'],
     standalone: false
 })
-export class ConfigurationUploadComponent implements OnInit{
+export class ConfigurationUploadComponent {
   /**
    * Registered name of the configuration to upload.
    * If null, all configurations in the selected will be uploaded
    */
-  @Input() public configurationName: string | null = null;
-  @Input() public configurationNameObservable: Observable<StepConfiguration> | null = null;
+  @Input() public configurationName!: string;
   @Input() public disabled: boolean = false;
   @Output() onUpload: EventEmitter<ImportPipeData[] | null> = new EventEmitter();
 
-    protected configFile: File | null = null;
     protected isFileTypeInvalid: boolean = false;
 
     constructor(
         private readonly errorHandlingService: ErrorHandlingService,
         private configurationService: ConfigurationService,
-        private dialog: MatDialog,
     ) {
     }
 
-  ngOnInit() {
-      if (this.configurationNameObservable !== null) {
-          this.configurationNameObservable.subscribe({
-              next: value => {
-                  this.configurationName = value.configurationName;
-              }
-          });
-      }
-  }
-
-  /**
-   * Opens the import dialog.
-   * @param templateRef Reference to the dialog.
-   */
-  openDialog(templateRef: TemplateRef<any>) {
-    this.dialog.open(templateRef, {
-      width: '60%'
-    }).afterClosed().subscribe({
-        next: () => {
-            this.configFile = null;
-        }
-    });
-  }
-
-  /**
-   * Closes the import dialog.
-   */
-  public closeDialog() {
-    this.dialog.closeAll();
-  }
-
     /**
-     * Activates/deactivates the upload button if an input is made.
-     * @param event Input event of the file input.
-     * @protected
+     * Handles the file upload event.
+     * First checks if a file is selected and if the file type is valid (yaml or yml).
+     * Then uploads the selected configuration file.
+     * Uses the setConfigCallback function to update the configuration in the application.
+     * If configured, stores the configuration under the configured name into the database.
      */
-  protected updateUploadButton(event: Event) {
-      const input = event.target as HTMLInputElement;
-      const hasFile = input.files != null && input.files.length > 0;
-
-      this.configFile = null;
-      this.isFileTypeInvalid = false;
-
-      if (hasFile) {
-          const file = input.files![0];
-          const fileExtension = this.getFileExtension(file);
-
-          if (fileExtension == null || !["yaml", "yml"].includes(fileExtension)) {
-              this.isFileTypeInvalid = true;
-          } else {
-              this.configFile =  file;
-          }
-      }
-  }
-
-  /**
-   * Uploads the selected configuration file.
-   * If the configuration name of this component is set, only this configuration will be uploaded.
-   * Uses the setConfigCallback function to update the configuration in the application.
-   * If configured, stores the configuration under the configured name into the database.
-   */
-  uploadConfiguration() {
-    if (!this.configFile) {
-      console.error("No file selected. This should be prevented by disabling the upload button.");
-      return;
-    }
-
-    const included = [];
-    if (this.configurationName !== null) {
-      included.push(this.configurationName);
-    } else {
-      for (const config of this.configurationService.getRegisteredConfigurations()) {
-        if ((document.getElementById(config.name + "-input") as HTMLInputElement).checked) {
-          included.push(config.name);
+    uploadConfiguration(fileList: FileList | null): void {
+        if (fileList === null || fileList.length === 0) {
+            return;
         }
-      }
-    }
 
-      this.configurationService.uploadAllConfigurations(this.configFile, included).subscribe({
-          next: result => {
-              this.onUpload.emit(result);
-          },
-          error: error => {
-              this.errorHandlingService.addError(error, "Could not upload configuration.");
-          },
-      });
-    this.closeDialog();
-  }
+        const file = fileList[0];
+        const fileExtension = this.getFileExtension(file);
+
+        if (fileExtension == null || !["yaml", "yml"].includes(fileExtension)) {
+            this.isFileTypeInvalid = true;
+            return;
+        } else {
+            this.isFileTypeInvalid = false;
+        }
+
+        const included = [this.configurationName];
+
+        this.configurationService.uploadAllConfigurations(file, included).subscribe({
+            next: result => {
+                this.onUpload.emit(result);
+            },
+            error: error => {
+                this.errorHandlingService.addError(error, "Could not upload configuration.");
+            },
+        });
+    }
 
     /**
      * Extracts the file extension from the given file.

da4f0a7fcaefa4d0da4370e12dbae2c5975309a9
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-06-05 12:59:18 +0200

    Added password validator to the frontend

--- cinnamon-platform/cinnamon-frontend/src/app/features/auth/pages/register/register.component.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/features/auth/pages/register/register.component.ts
@@ -1,11 +1,11 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { Component, OnInit, TemplateRef } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
 import { MatDialog } from "@angular/material/dialog";
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, tap } from "rxjs";
 import { TitleService } from "src/app/core/services/title-service.service";
-import { AppConfig, AppConfigService } from "src/app/shared/services/app-config.service";
+import { AppConfig, AppConfigService, PasswordRequirements } from "src/app/shared/services/app-config.service";
 import { ErrorHandlingService } from "src/app/shared/services/error-handling.service";
 import { UserService } from "src/app/shared/services/user.service";
 
@@ -34,27 +34,28 @@ export class RegisterComponent implements OnInit {
         private readonly titleService: TitleService,
         private readonly userService: UserService,
     ) {
-        this.registerForm = new FormGroup<RegisterForm>({
-            email: new FormControl<string>("", {
-                nonNullable: true,
-                validators: [Validators.required],
-            }),
-            password: new FormControl<string>("", {
-                nonNullable: true,
-                validators: [Validators.required],
-            }),
-            passwordRepeated: new FormControl<string>("", {
-                nonNullable: true,
-                validators: [Validators.required],
-            }),
-        });
-
         this.titleService.setPageTitle("Create new project");
     }
 
-
     public ngOnInit(): void {
-        this.appConfig$ = this.appConfigService.appConfig$;
+        this.appConfig$ = this.appConfigService.appConfig$.pipe(
+            tap(appConfig => {
+                this.registerForm = new FormGroup<RegisterForm>({
+                    email: new FormControl<string>("", {
+                        nonNullable: true,
+                        validators: [Validators.required],
+                    }),
+                    password: new FormControl<string>("", {
+                        nonNullable: true,
+                        validators: [this.passwordRequirementsValidator(appConfig.passwordRequirements)],
+                    }),
+                    passwordRepeated: new FormControl<string>("", {
+                        nonNullable: true,
+                        validators: [Validators.required],
+                    }),
+                });
+            }),
+        );
     }
 
     /**
@@ -64,6 +65,37 @@ export class RegisterComponent implements OnInit {
      */
     protected openDialog(ref: TemplateRef<any>) {
         this.matDialog.open(ref);
+    }
+
+    /**
+     * Creates the error message for the password field.
+     * @protected
+     */
+    protected createPasswordErrorMessage(): string | null {
+        const control = this.registerForm.controls["password"] as FormControl;
+        if (control.errors == null) {
+            return null;
+        }
+
+        const errors: string[] = [];
+
+        if (control.hasError('length')) {
+            errors.push(`be at least ${control.getError('length').minLength} characters long`);
+        }
+        if (control.hasError('digit')) {
+            errors.push('contain at lest one digit')
+        }
+        if (control.hasError('lowercase')) {
+            errors.push(`contain at least one lowercase character`);
+        }
+        if (control.hasError('uppercase')) {
+            errors.push(`contain at least one uppercase character`);
+        }
+        if (control.hasError('specialChar')) {
+            errors.push(`contain at least one special character`);
+        }
+
+        return "Password must " + errors.join(", ");
     }
 
     onSubmit(): void {
@@ -86,4 +118,59 @@ export class RegisterComponent implements OnInit {
     handleRegisterFailed(error: HttpErrorResponse) {
         this.errorHandlingService.addError(error);
     }
+
+    /**
+     * Creates a password validator for the given password requirements.
+     * @param passwordRequirements The password requirements.
+     * @private
+     */
+    private passwordRequirementsValidator(passwordRequirements: PasswordRequirements): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            if (typeof control.value !== "string") {
+                return null;
+            }
+
+            let hasLength = control.value.length >= passwordRequirements.minLength
+
+            const constraints = passwordRequirements.constraints;
+            let hasLowercase = !constraints.includes('LOWERCASE');
+            let hasDigit = !constraints.includes('DIGIT');
+            let hasSpecialChar = !constraints.includes('SPECIAL_CHAR');
+            let hasUppercase = !constraints.includes('UPPERCASE');
+
+            for (let i = 0; i < control.value.length; i++) {
+                const c = control.value.charAt(i);
+
+                if (/\p{N}/u.test(c)) {
+                    hasDigit = true;
+                } else if (/\p{Ll}/u.test(c)) {
+                    hasLowercase = true;
+                } else if (/\p{Lu}/u.test(c)) {
+                    hasUppercase = true;
+                } else {
+                    hasSpecialChar = true;
+                }
+            }
+
+            const v: Record<string, any> = {};
+            if (!hasLength) {
+                v['length'] = {minLength: passwordRequirements.minLength};
+            }
+            if (!hasDigit) {
+                v['digit'] = {};
+            }
+            if (!hasLowercase) {
+                v['lowercase'] = {};
+            }
+            if (!hasUppercase) {
+                v['uppercase'] = {};
+            }
+            if (!hasSpecialChar) {
+                v['specialChar'] = {};
+            }
+
+            return hasLength && hasLowercase && hasDigit && hasSpecialChar && hasUppercase ? null : v;
+        }
+    }
+
 }

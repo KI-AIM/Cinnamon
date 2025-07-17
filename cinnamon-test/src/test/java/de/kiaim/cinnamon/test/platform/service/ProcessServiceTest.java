39d6b5bef2d187eb455fd6a02f797b1a33d74dbe
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-07-17 15:42:36 +0200

    Removed OS language dependent assert

--- cinnamon-test/src/test/java/de/kiaim/cinnamon/test/platform/service/ProcessServiceTest.java
+++ cinnamon-test/src/test/java/de/kiaim/cinnamon/test/platform/service/ProcessServiceTest.java
@@ -125,7 +125,6 @@ public class ProcessServiceTest extends ContextRequiredTest {
 		var message = updatedExecutionStep.getProcess(0).getStatus();
 		assertNotNull(message, "Status message should not be null!");
 		assertTrue(message.startsWith("Failed to fetch the status!"), "Unexpected start of the error message: '" + message + "'");
-		assertTrue(message.contains("Connection refused:"), "Unexpected error message: '" + message + "'");
 		assertEquals("localhost/127.0.0.1:" + mockBackEndPort, message.substring(message.lastIndexOf("localhost/")),
 		             "Unexpected end of the error message: '" + message + "'");
 	}

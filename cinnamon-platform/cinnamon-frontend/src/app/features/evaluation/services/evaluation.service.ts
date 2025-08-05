970ac6c345b190756024118e8cb600dcccdc3c50
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-08-05 14:52:16 +0200

    Lock execution pages if previous stages are not finished

--- cinnamon-platform/cinnamon-frontend/src/app/features/evaluation/services/evaluation.service.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/features/evaluation/services/evaluation.service.ts
@@ -20,7 +20,7 @@ export class EvaluationService extends ExecutionStepService {
         super(errorHandlingService, http, stateManagementService, statusService);
     }
 
-    protected override getStageName(): string {
+    protected override getStageName2(): string {
         return "evaluation";
     }
 

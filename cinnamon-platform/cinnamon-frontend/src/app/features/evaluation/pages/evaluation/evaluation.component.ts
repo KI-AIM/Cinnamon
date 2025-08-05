7b7e2f11298969455b7649a938a88b9e92961d35
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-08-05 14:31:29 +0200

    Refactored evaluation component

--- cinnamon-platform/cinnamon-frontend/src/app/features/evaluation/pages/evaluation/evaluation.component.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/features/evaluation/pages/evaluation/evaluation.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { MatExpansionPanel } from "@angular/material/expansion";
+import { StateManagementService } from "@core/services/state-management.service";
 import { ErrorHandlingService } from "@shared/services/error-handling.service";
 import { EvaluationService } from "../../services/evaluation.service";
 import { TitleService } from "@core/services/title-service.service";
 import { ProcessStatus } from "@core/enums/process-status";
-import { Observable, tap } from "rxjs";
+import { combineLatest, map, Observable, tap } from "rxjs";
 import { StatisticsService } from "@shared/services/statistics.service";
 import {
     StatisticsResponse, UtilityData,
@@ -15,7 +16,7 @@ import {
 import { ExecutionStep } from "@shared/model/execution-step";
 import { RiskEvaluation } from '@shared/model/risk-evaluation';
 import { ProjectConfigurationService } from "@shared/services/project-configuration.service";
-import { ProjectSettings } from "@shared/model/project-settings";
+import { MetricSettings } from "@shared/model/project-settings";
 import { StageDefinition } from "@shared/services/execution-step.service";
 
 @Component({
@@ -30,26 +31,17 @@ export class EvaluationComponent implements OnInit {
     protected readonly UtilityMetricData2 = UtilityMetricData2;
     protected readonly UtilityMetricData3 = UtilityMetricData3;
 
-    protected stage$: Observable<ExecutionStep | null>;
-    protected stageDefinition$: Observable<StageDefinition>;
-
     protected statistics$: Observable<StatisticsResponse>;
 
     protected risks$: Observable<RiskEvaluation>;
     protected risks2$: Observable<any>;
 
-    protected riskMetrics: {
-        label: string,
-        key: keyof RiskEvaluation,
-        hasAttackRate: boolean
-    }[] = [
-        { label: "Linkage", key: "linkage_health_risk", hasAttackRate: true },
-        // { label: "Singling-out univariate", key: "univariate_singling_out_risk", hasAttackRate: true },
-        // { label: "Singling-out multivariate", key: "multivariate_singling_out_risk", hasAttackRate: true },
-        // { label: "Average Attribute Inference", key: "inference_average_risk", hasAttackRate: true }
-    ];
-
-    protected projectConfig$: Observable<ProjectSettings>;
+    protected pageData$: Observable<{
+        locked: boolean,
+        metricConfiguration: MetricSettings,
+        stage: ExecutionStep | null,
+        stageDefinition: StageDefinition,
+    }>
 
     @ViewChildren('statusPanel') private statusPanels: QueryList<MatExpansionPanel>;
 
@@ -59,6 +51,7 @@ export class EvaluationComponent implements OnInit {
     constructor(
         private readonly errorHandlingService: ErrorHandlingService,
         protected readonly evaluationService: EvaluationService,
+        private readonly stateManagementService: StateManagementService,
         protected readonly statisticsService: StatisticsService,
         private readonly titleService: TitleService,
         protected readonly projectConfigService: ProjectConfigurationService,
@@ -67,53 +60,57 @@ export class EvaluationComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.projectConfig$ = this.projectConfigService.projectSettings$;
+        this.pageData$ = combineLatest({
+            locked: this.stateManagementService.currentStepLocked$.pipe(
+                map(value => value.isLocked),
+            ),
+            metricConfiguration: this.projectConfigService.projectSettings$.pipe(
+                map(value => value.metricConfiguration),
+            ),
+            stage: this.evaluationService.status$.pipe(
+                tap(value => {
+                    // Create an error notification
+                    if (this.statusPanels && value && value.status === ProcessStatus.ERROR &&
+                        this.currentStatus !== ProcessStatus.ERROR && this.currentStatus !== null) {
+
+                        let jobIndex = null;
+                        for (let i = 0; i < value.processes.length; i++) {
+                            if (value.processes[i].externalProcessStatus === ProcessStatus.ERROR) {
+                                jobIndex = i;
+                                break;
+                            }
+                        }
+
+                        if (jobIndex !== null) {
+                            this.statusPanels.get(jobIndex)?.open();
+                            const name = this.getJobName(value.processes[jobIndex].step);
+                            this.errorHandlingService.addError(`The ${name} process failed. See the status panel for more information.`);
+                        }
+                    }
+                }),
+                tap(value => {
+                    // Open the status panel when the next job starts
+                    if (value && this.statusPanels) {
+                        if (value.currentProcessIndex === null && value.status === ProcessStatus.FINISHED) {
+                            this.statusPanels.forEach(panel => panel.close());
+                        }
+
+                        if (value.currentProcessIndex !== null && value.currentProcessIndex !== this.currentJob) {
+                            this.statusPanels.forEach(panel => panel.close());
+                            this.statusPanels.get(value.currentProcessIndex)?.open();
+                        }
+
+                        // Update the current status
+                        this.currentJob = value.currentProcessIndex;
+                        this.currentStatus = value.status;
+                    }
+                }),
+            ),
+            stageDefinition: this.evaluationService.fetchStageDefinition$(),
+        });
 
         this.risks$ = this.statisticsService.fetchRisks();
         this.risks2$ = this.statisticsService.fetchRisks2();
-        this.stage$ = this.evaluationService.status$;
-
-        this.stage$ = this.evaluationService.status$.pipe(
-            tap(value => {
-                // Create an error notification
-                if (this.statusPanels && value && value.status === ProcessStatus.ERROR &&
-                    this.currentStatus !== ProcessStatus.ERROR && this.currentStatus !== null) {
-
-                    let jobIndex = null;
-                    for (let i = 0; i < value.processes.length; i++) {
-                        if (value.processes[i].externalProcessStatus === ProcessStatus.ERROR) {
-                            jobIndex = i;
-                            break;
-                        }
-                    }
-
-                    if (jobIndex !== null) {
-                        this.statusPanels.get(jobIndex)?.open();
-                        const name = this.getJobName(value.processes[jobIndex].step);
-                        this.errorHandlingService.addError(`The ${name} process failed. See the status panel for more information.`);
-                    }
-                }
-            }),
-            tap(value => {
-                // Open the status panel when the next job starts
-                if (value && this.statusPanels) {
-                    if (value.currentProcessIndex === null && value.status === ProcessStatus.FINISHED) {
-                        this.statusPanels.forEach(panel => panel.close());
-                    }
-
-                    if (value.currentProcessIndex !== null && value.currentProcessIndex !== this.currentJob) {
-                        this.statusPanels.forEach(panel => panel.close());
-                        this.statusPanels.get(value.currentProcessIndex)?.open();
-                    }
-
-                    // Update the current status
-                    this.currentJob = value.currentProcessIndex;
-                    this.currentStatus = value.status;
-                }
-            }),
-        );
-
-        this.stageDefinition$ = this.evaluationService.fetchStageDefinition$();
         this.statistics$ = this.statisticsService.fetchResult();
         this.evaluationService.fetchStatus();
     }

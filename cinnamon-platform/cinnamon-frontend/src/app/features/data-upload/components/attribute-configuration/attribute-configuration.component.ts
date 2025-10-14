2a8afaf80f25a8e3a44d5ccee6c64ce246cd6d71
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-10-14 16:32:27 +0200

    Added inforamtion to the data configuration

--- cinnamon-platform/cinnamon-frontend/src/app/features/data-upload/components/attribute-configuration/attribute-configuration.component.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/features/data-upload/components/attribute-configuration/attribute-configuration.component.ts
@@ -1,9 +1,9 @@
 import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { MatDialog } from "@angular/material/dialog";
 import { FileType } from "@shared/model/file-configuration";
-import { List } from 'src/app/core/utils/list';
-import { DataScale } from 'src/app/shared/model/data-scale';
-import { DataType } from 'src/app/shared/model/data-type';
+import { DataScaleMetadata } from 'src/app/shared/model/data-scale';
+import { DataTypeMetadata } from 'src/app/shared/model/data-type';
 
 @Component({
     selector: 'app-attribute-configuration',
@@ -21,11 +21,15 @@ export class AttributeConfigurationComponent implements AfterViewInit {
     @Output() onInput = new EventEmitter<any>();
 
     protected readonly FileType = FileType;
-    protected readonly DataType = DataType;
 
     private oldName: string = "";
 
-    constructor() { }
+    protected readonly DataScaleMetadata = DataScaleMetadata;
+    protected readonly DataTypeMetadata = DataTypeMetadata;
+
+    constructor(
+        protected dialog: MatDialog
+    ) { }
 
     ngAfterViewInit(): void {
         this.columnConfigurationForm.controls['name'].valueChanges.subscribe((newValue) => {
@@ -36,16 +40,6 @@ export class AttributeConfigurationComponent implements AfterViewInit {
                 this.columnConfigurationForm.controls['name'].markAsTouched();
             }
         });
-    }
-
-    getDataTypes(): List<String> {
-        return new List<String>(Object.keys(DataType));
-    }
-
-    getDataScales(): List<String> {
-        const scales = Object.keys(DataScale).filter(x => !(parseInt(x) >= 0));
-
-        return new List<String>(scales);
     }
 
     protected parseInt(value: String): Number {
@@ -59,4 +53,5 @@ export class AttributeConfigurationComponent implements AfterViewInit {
             this.columnConfigurationForm.controls[field].patchValue(trimmedValue);
         }
     }
+
 }
--- cinnamon-platform/cinnamon-frontend/src/app/shared/model/data-type.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/shared/model/data-type.ts
@@ -8,14 +8,62 @@ export enum DataType {
 	UNDEFINED = "UNDEFINED",
 }
 
+/**
+ * Metadata for data types.
+ */
+interface DataTypeAttributes {
+    /**
+     * Name for displaying the type in the UI.
+     */
+    displayName: string;
+    /**
+     * If the user can select this type.
+     */
+    selectable: boolean;
+}
+
+/**
+ * Additional metadata for each data type.
+ */
+export const DataTypeMetadata: Record<DataType, DataTypeAttributes> = {
+    [DataType.BOOLEAN]: {
+        displayName: "Boolean",
+        selectable: true,
+    },
+    [DataType.DATE_TIME]: {
+        displayName: "Date & Time",
+        selectable: true,
+    },
+    [DataType.DECIMAL]: {
+        displayName: "Decimal",
+        selectable: true,
+    },
+    [DataType.INTEGER]: {
+        displayName: "Integer",
+        selectable: true,
+    },
+    [DataType.STRING]: {
+        displayName: "String",
+        selectable: true,
+    },
+    [DataType.DATE]: {
+        displayName: "Date",
+        selectable: true,
+    },
+    [DataType.UNDEFINED]: {
+        displayName: "Undefined",
+        selectable: false,
+    }
+};
+
 export function dataTypeFromString(value: String): DataType {
-	switch (value) {
-		case "BOOLEAN": return DataType.BOOLEAN
-		case "DATE_TIME": return DataType.DATE_TIME
-		case "DECIMAL": return DataType.DECIMAL
-		case "INTEGER": return DataType.INTEGER
-		case "STRING": return DataType.STRING
-		case "DATE": return DataType.DATE
-		default: return DataType.UNDEFINED
-	}
+    switch (value) {
+        case "BOOLEAN": return DataType.BOOLEAN
+        case "DATE_TIME": return DataType.DATE_TIME
+        case "DECIMAL": return DataType.DECIMAL
+        case "INTEGER": return DataType.INTEGER
+        case "STRING": return DataType.STRING
+        case "DATE": return DataType.DATE
+        default: return DataType.UNDEFINED
+    }
 }

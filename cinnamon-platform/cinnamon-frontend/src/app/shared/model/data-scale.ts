771bcdcfdb6d3eb92632d57e989fe4576c192083
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-10-14 16:32:27 +0200

    Added information to the data configuration

--- cinnamon-platform/cinnamon-frontend/src/app/shared/model/data-scale.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/shared/model/data-scale.ts
@@ -1,7 +1,38 @@
 export enum DataScale {
-    DATE,
-    NOMINAL,
-    ORDINAL,
-    INTERVAL,
-    RATIO,
+    DATE = "DATE",
+    NOMINAL = "NOMINAL",
+    ORDINAL = "ORDINAL",
+    INTERVAL = "INTERVAL",
+    RATIO = "RATIO",
+}
+
+/**
+ * Metadata for data scales.
+ */
+interface DataScaleAttributes {
+    /**
+     * Name for displaying the scale in the UI.
+     */
+    displayName: string;
+}
+
+/**
+ * Additional metadata for each data type.
+ */
+export const DataScaleMetadata: Record<DataScale, DataScaleAttributes> = {
+    [DataScale.NOMINAL]: {
+        displayName: 'Nominal',
+    },
+    [DataScale.ORDINAL]: {
+        displayName: 'Ordinal',
+    },
+    [DataScale.INTERVAL]: {
+        displayName: 'Interval',
+    },
+    [DataScale.RATIO]: {
+        displayName: 'Ratio',
+    },
+    [DataScale.DATE]: {
+        displayName: 'Date',
+    },
 }

3220a2333aa74db59dc9af913d06730a72ac0ae1
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-10-17 13:23:09 +0200

    Removed spec files for pipes

--- cinnamon-platform/cinnamon-frontend/src/app/shared/pipes/metric-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MetricFilterPipe } from './metric-filter.pipe';
-
-describe('MetricFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MetricFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- cinnamon-platform/cinnamon-frontend/src/app/shared/pipes/statistics-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StatisticsFilterPipe } from './statistics-filter.pipe';
-
-describe('StatisticsFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StatisticsFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

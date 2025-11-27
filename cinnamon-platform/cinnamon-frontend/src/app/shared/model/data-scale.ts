40f00cabf5e6503f1104f3f37952e84d00764a2a
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2025-11-27 10:28:36 +0100

    Added column for supported types in info table for scales

--- cinnamon-platform/cinnamon-frontend/src/app/shared/model/data-scale.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/shared/model/data-scale.ts
@@ -1,3 +1,5 @@
+import { DataType } from "@shared/model/data-type";
+
 export enum DataScale {
     DATE = "DATE",
     NOMINAL = "NOMINAL",
@@ -11,6 +13,10 @@ export enum DataScale {
  */
 interface DataScaleAttributes {
     /**
+     * List of data types for which the scale can be selected.
+     */
+    applicableTo: DataType[];
+    /**
      * Name for displaying the scale in the UI.
      */
     displayName: string;
@@ -21,18 +27,23 @@ interface DataScaleAttributes {
  */
 export const DataScaleMetadata: Record<DataScale, DataScaleAttributes> = {
     [DataScale.NOMINAL]: {
+        applicableTo: [DataType.BOOLEAN, DataType.DECIMAL, DataType.INTEGER, DataType.STRING],
         displayName: 'Nominal',
     },
     [DataScale.ORDINAL]: {
+        applicableTo: [DataType.DECIMAL, DataType.INTEGER, DataType.STRING],
         displayName: 'Ordinal',
     },
     [DataScale.INTERVAL]: {
+        applicableTo: [DataType.DECIMAL, DataType.INTEGER],
         displayName: 'Interval',
     },
     [DataScale.RATIO]: {
+        applicableTo: [DataType.DECIMAL],
         displayName: 'Ratio',
     },
     [DataScale.DATE]: {
+        applicableTo: [DataType.DATE, DataType.DATE_TIME],
         displayName: 'Date',
     },
 }

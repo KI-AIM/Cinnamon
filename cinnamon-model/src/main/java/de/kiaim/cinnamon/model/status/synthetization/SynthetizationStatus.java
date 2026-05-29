c0abe47f903de861a1b15bc10e5565287dd4e684
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2026-05-29 12:50:34 +0200

    Fixed synthetization status test

--- cinnamon-model/src/main/java/de/kiaim/cinnamon/model/status/synthetization/SynthetizationStatus.java
+++ cinnamon-model/src/main/java/de/kiaim/cinnamon/model/status/synthetization/SynthetizationStatus.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Getter @Setter
 public class SynthetizationStatus {
 
-	private Map<String, SynthetizationComponentStatus> components;
+	private Map<String, SynthetizationComponentStatus> components = new HashMap<>();
 
 	@JsonProperty(value = "session_key")
 	private String sessionKey;

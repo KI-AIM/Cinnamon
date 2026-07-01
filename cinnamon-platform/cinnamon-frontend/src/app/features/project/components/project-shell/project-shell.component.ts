b2322af7c1d524fcb724a0f98dd0b7d7daae9e61
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2026-07-01 17:02:24 +0200

    (WIP) Open projects

--- cinnamon-platform/cinnamon-frontend/src/app/features/project/components/project-shell/project-shell.component.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/features/project/components/project-shell/project-shell.component.ts
@@ -3,12 +3,12 @@ import { ActivatedRoute } from "@angular/router";
 import { StateManagementService } from "@core/services/state-management.service";
 import { ProjectService } from "@shared/services/project.service";
 import { StatusService } from "@shared/services/status.service";
+import { switchMap } from "rxjs";
 
 @Component({
     selector: 'app-project-shell',
     standalone: false,
     templateUrl: './project-shell.component.html',
-    styleUrl: './project-shell.component.less'
 })
 export class ProjectShellComponent implements OnInit {
 
@@ -24,14 +24,17 @@ export class ProjectShellComponent implements OnInit {
         const projectId = this.route.snapshot.params['projectId'];
         console.log("Project ID: " + projectId);
         if (projectId) {
-            this.projectService.openProjectId(projectId).subscribe({
-                    next: (project) => {
+            this.projectService.openProjectId(projectId).pipe(
+                switchMap((project) => {
+                    return this.statusService.updateStatus(project.id);
+                }),
+            ).subscribe({
+                    next: (status) => {
+                        this.stateManagementService.routeToCurrentStep(projectId, status);
                     },
                 }
             );
         }
-
-        this.stateManagementService.fetchAndRouteToCurrentStep();
     }
 
 }

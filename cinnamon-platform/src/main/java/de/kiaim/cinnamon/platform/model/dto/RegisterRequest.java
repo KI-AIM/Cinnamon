3468e37609ae6e8f598612733c3a4d13bc2aa51d
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2026-07-17 13:00:58 +0200

    Added annotation for username validation

--- cinnamon-platform/src/main/java/de/kiaim/cinnamon/platform/model/dto/RegisterRequest.java
+++ cinnamon-platform/src/main/java/de/kiaim/cinnamon/platform/model/dto/RegisterRequest.java
@@ -1,9 +1,8 @@
 package de.kiaim.cinnamon.platform.model.dto;
 
-import de.kiaim.cinnamon.platform.model.validation.UsernameAvailable;
+import de.kiaim.cinnamon.platform.model.validation.Username;
 import de.kiaim.cinnamon.platform.model.validation.PasswordRequirements;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,8 +14,7 @@ import lombok.NoArgsConstructor;
 public class RegisterRequest implements MatchingPasswords {
 
 	@Schema(description = "Username of the user.", example = "john_doe")
-	@NotBlank
-	@UsernameAvailable
+	@Username
 	private String username;
 
 	@Schema(description = "Password of the user.", example = "changeme")

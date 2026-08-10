49d00c81303ceeb0423c17194489fa16568fbf93
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2026-08-10 15:49:14 +0200

    Fixed some frontend tests

--- cinnamon-platform/cinnamon-frontend/src/app/features/risk-assessment/services/risk-assessment.service.spec.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/features/risk-assessment/services/risk-assessment.service.spec.ts
@@ -1,18 +1,23 @@
 import { HttpClient } from '@angular/common/http';
+import { ProjectService } from "@shared/services/project.service";
 import { RiskAssessmentService } from './risk-assessment.service';
 import { ConfigurationService } from 'src/app/shared/services/configuration.service';
 
 describe('RiskAssessmentService', () => {
-  let service: RiskAssessmentService;
+    let service: RiskAssessmentService;
 
-  beforeEach(() => {
-    service = new RiskAssessmentService(
-      new ConfigurationService({} as HttpClient),
-      {} as HttpClient,
-    );
-  });
+    beforeEach(() => {
+        const httpClient = {} as HttpClient; // Mock HttpClient
+        const projectService = new ProjectService(httpClient);
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        service = new RiskAssessmentService(
+            new ConfigurationService(httpClient, projectService),
+            httpClient,
+            projectService,
+        );
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
--- cinnamon-platform/cinnamon-frontend/src/app/shared/services/data.service.spec.ts
+++ cinnamon-platform/cinnamon-frontend/src/app/shared/services/data.service.spec.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
+import { ProjectService } from "@shared/services/project.service";
 import { DataService } from "./data.service";
 
 describe("DataService", () => {
 	let service: DataService;
 
 	beforeEach(() => {
-		service = new DataService({} as HttpClient);
+        const httpClient = {} as HttpClient; // Mock HttpClient
+        const projectService = new ProjectService(httpClient);
+		service = new DataService(httpClient, projectService);
 	});
 
 	it("should be created", () => {

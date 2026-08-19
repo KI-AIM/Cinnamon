0eb1ccf5b899473e8ce4e2c0aeae6c04dca6d887
Daniel Preciado-Marquez <daniel.preciado-marquez@uni-muenster.de>
2026-08-19 10:27:44 +0200

    Fixed database column types

--- cinnamon-platform/src/main/java/de/kiaim/cinnamon/platform/model/entity/FileCompatibilityEntity.java
+++ cinnamon-platform/src/main/java/de/kiaim/cinnamon/platform/model/entity/FileCompatibilityEntity.java
@@ -1,11 +1,14 @@
 package de.kiaim.cinnamon.platform.model.entity;
 
 import de.kiaim.cinnamon.model.configuration.data.file.FileType;
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 import org.springframework.lang.Nullable;
 
 import java.util.HashSet;
@@ -25,12 +28,16 @@ public class FileCompatibilityEntity {
     /**
      * The file types that are compatible with the file.
      */
+    @Type(JsonType.class)
+    @Column(columnDefinition = "json")
     private final Set<FileType> compatibleFileTypes = new HashSet<>();
 
     /**
      * The resource types contained in the FHIR bundle.
      * Only relevant for files of type {@link de.kiaim.cinnamon.model.configuration.data.file.FileType#FHIR}
      */
+    @Type(JsonType.class)
+    @Column(columnDefinition = "json")
     @Nullable
     private Set<String> fhirResourceTypes = null;
 
